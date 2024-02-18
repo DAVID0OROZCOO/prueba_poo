e25822ce4a386f1305b49b9be1ba677227801423
Cristian Arias <ariascristian019@gmail.com>
2024-02-18 13:00:22 -0500

    Quinto programa, fahrenheit a celcius

---
+++ fahrenheit_celcius.py
@@ -0,0 +1,6 @@
+
+def fahrenheit_a_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+fahrenheit = float(input("Ingrese la temperatura en grados Fahrenheit: "))
+print("La temperatura en grados Celsius es:", fahrenheit_a_celsius(fahrenheit))
