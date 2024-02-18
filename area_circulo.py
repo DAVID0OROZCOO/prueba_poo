13cde6c2a4145679b6f15a4f5fdfb2ad12892bdb
Cristian Arias <ariascristian019@gmail.com>
2024-02-18 12:54:55 -0500

    Primer programa, areas circulo

---
+++ area_circulo.py
@@ -0,0 +1,7 @@
+import math
+
+def calcular_area_circulo(radio):
+    return math.pi * radio**2
+
+radio = float(input("Ingrese el radio del círculo: "))
+print("El área del círculo es:", calcular_area_circulo(radio))
