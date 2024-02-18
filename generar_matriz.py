658b9bd0b06668ad6d65885808dc9ca5aecaa205
Cristian Arias <ariascristian019@gmail.com>
2024-02-18 13:04:29 -0500

    Noveno programa, generar matriz

---
+++ generar_matriz.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+filas = int(input("Ingrese el número de filas: "))
+columnas = int(input("Ingrese el número de columnas: "))
+matriz = np.random.randint(1, 100, size=(filas, columnas))
+print("Matriz generada:")
+print(matriz)
