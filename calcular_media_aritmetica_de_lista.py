66ca7d7c05b233f29991c1c19e2b4fd25dbea83e
Cristian Arias <ariascristian019@gmail.com>
2024-02-18 13:08:28 -0500

    Catorceavo programa, media aritmetica de lista

---
+++ calcular_media_aritmetica_de_lista.py
@@ -0,0 +1,5 @@
+def media_aritmetica(lista):
+    return sum(lista) / len(lista)
+
+numeros = [1, 2, 3, 4, 5]  # Ejemplo de lista
+print("La media aritmética de la lista es:", media_aritmetica(numeros))
