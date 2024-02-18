ca82253cb20b98aa087092fd6b3f0282e092d3b5
Cristian Arias <ariascristian019@gmail.com>
2024-02-18 12:57:26 -0500

    Tercer programa, lista numeros randoms

---
+++ lista_numeros_randoms.py
@@ -0,0 +1,5 @@
+import random
+
+cantidad_numeros = int(input("Ingrese la cantidad de números aleatorios a generar: "))
+numeros_aleatorios = [random.randint(1, 100) for _ in range(cantidad_numeros)]
+print("Lista de números aleatorios:", numeros_aleatorios)
